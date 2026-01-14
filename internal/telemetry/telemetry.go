a8c7df66cf5e2b78226beec88c252ef51a13c5d1
Jayesh Khairnar <jkhairnar@stud.hs-heilbronn.de>
2026-01-14 18:01:43 +0100

    feat: export traces to Google Cloud Trace

--- internal/telemetry/telemetry.go
+++ internal/telemetry/telemetry.go
@@ -9,6 +9,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	
 )
 
 func InitTracer(serviceName string) (func(context.Context) error, error) {
