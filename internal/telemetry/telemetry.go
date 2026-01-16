92e000a30260032a04b06945baa8361a239f6217
Jayeshkhairnar-git <jayeshkhairnar20003@gmail.com>
2026-01-16 15:26:19 +0100

    Update semconv library.go

--- internal/telemetry/telemetry.go
+++ internal/telemetry/telemetry.go
@@ -8,14 +8,11 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-    //semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
-	
 )
 
 func InitTracer(serviceName string) (func(context.Context) error, error) {
 	ctx := context.Background()
-
 
 	exporter, err := otlptracegrpc.New(
 		ctx,
@@ -25,12 +22,11 @@ func InitTracer(serviceName string) (func(context.Context) error, error) {
 		return nil, err
 	}
 
-
 	res, err := resource.New(
 		ctx,
 		resource.WithAttributes(
-			semconv.ServiceName(serviceName),
-			semconv.DeploymentEnvironment("cloudrun"),
+			semconv.ServiceNameKey.String(serviceName),
+			semconv.DeploymentEnvironmentNameKey.String("cloudrun"),
 		),
 	)
 	if err != nil {
@@ -49,4 +45,3 @@ func InitTracer(serviceName string) (func(context.Context) error, error) {
 
 	return tp.Shutdown, nil
 }
-
